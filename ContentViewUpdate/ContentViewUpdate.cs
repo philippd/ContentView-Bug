42e216cbd772ac79d6e91334b74ea3aedc64ffb8
Philipp Denzler <philippd@gmail.com>
2015-11-18 15:29:32 +0100

    Demo of ContentView bug

---
+++ ContentViewUpdate/ContentViewUpdate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace ContentViewUpdate
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new DemoPage();
+        }
+    }
+}
+
---
+++ ContentViewUpdate/DemoPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using System.Threading.Tasks;
+
+namespace ContentViewUpdate
+{
+    public partial class DemoPage : ContentPage
+    {
+        private int count = 0;
+
+        public DemoPage()
+        {
+            InitializeComponent();
+        }
+
+        async void UpdateContent (object sender, EventArgs e)
+        {            
+            this.testContent.Content = new Label { Text = $"Hello {count++}" };
+
+            this.testContent.IsVisible = true;
+        }
+
+        void HideContent (object sender, EventArgs e)
+        {
+            this.testContent.IsVisible = false;
+        }
+
+    }
+}
+
